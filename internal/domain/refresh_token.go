10d7094927ae1142ba3fb27340e339eed16cac92
syjoe <csy5501@gmail.com>
2026-01-29 22:34:19 +0900

    Add: connect DB to central-auth server

--- internal/domain/refresh_token.go
+++ internal/domain/refresh_token.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type RefreshToken struct {
-	UserID     int64
+	UserID     string
 	DeviceID   string
 	TokenHash  string
 	IssuedAt   time.Time
 	ExpiresAt  time.Time
-	LastUsedAt time.Time
-	UserAgent  string
-	IP         string
+	LastUsedAt *time.Time // nullable
+	UserAgent  *string
+	IP         *string
 	Revoked    bool
 }
